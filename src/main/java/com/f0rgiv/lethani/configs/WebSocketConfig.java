6b4932a31967a3c43b28fc230eee2ab3df36a61c
Garhett <garhett.l.morgan@gmail.com>
2021-05-21 16:33:26 -0700

    added about us page

--- src/main/java/com/f0rgiv/lethani/configs/WebSocketConfig.java
+++ src/main/java/com/f0rgiv/lethani/configs/WebSocketConfig.java
@@ -7,18 +7,18 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/gameMessage");
-        config.setApplicationDestinationPrefixes("/");
-    }
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry config) {
+//        config.enableSimpleBroker("/gameMessage");
+//        config.setApplicationDestinationPrefixes("/");
+//    }
 
 //    @Override
 //    public void registerStompEndPoints(StompEndpointRegistry registry) {
 //        registry.addEndpoint("/messaging").withSockJS();
 //    }
-}
+//}
--- src/main/java/com/f0rgiv/lethani/configs/WebSocketSecurityConfig.java
+++ src/main/java/com/f0rgiv/lethani/configs/WebSocketSecurityConfig.java
@@ -5,21 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 
-@Configuration
-public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
-
-    @Override
-    protected boolean sameOriginDisabled() {
-        return true;
-    }
-
-    @Override
-    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
-        System.out.println("configure websocket");
-        messages
-                .nullDestMatcher().authenticated()
-                .simpDestMatchers("/**").permitAll()
-                .simpSubscribeDestMatchers("/**").permitAll()
-                .anyMessage().permitAll();
-    }
-}
+//@Configuration
+//public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+//
+//    @Override
+//    protected boolean sameOriginDisabled() {
+//        return true;
+//    }
+//
+//    @Override
+//    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+//        System.out.println("configure websocket");
+//        messages
+//                .nullDestMatcher().authenticated()
+//                .simpDestMatchers("/**").permitAll()
+//                .simpSubscribeDestMatchers("/**").permitAll()
+//                .anyMessage().permitAll();
+//    }
+//}
