250d03f4d3d00a09245bf2fc5e4932422063d3fe
James Mansour <jamestmansour@outlook.com>
2021-05-21 15:40:28 -0700

    Add basic queries to appUserRepository

--- src/main/java/com/f0rgiv/lethani/repositories/AppUserRepository.java
+++ src/main/java/com/f0rgiv/lethani/repositories/AppUserRepository.java
@@ -1,4 +1,12 @@
 package com.f0rgiv.lethani.repositories;
 
-public interface AppUserRepository {
+import com.f0rgiv.lethani.models.AppUser;
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepository extends JpaRepositoryImplementation<AppUser, Long> {
+    AppUser findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
