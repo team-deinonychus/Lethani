74a06cc51cdaf4d9a97785247b0e8c081774a3c0
James Mansour <jamestmansour@outlook.com>
2021-05-21 10:02:21 -0700

    Adds initial Spring package

---
+++ src/test/java/com/f0rgiv/lethani/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f0rgiv.lethani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
