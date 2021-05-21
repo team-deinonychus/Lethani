0e72f8c14d5f8f48e9ddc80c7bd2dcc30cddd4dd
Garhett <garhett.l.morgan@gmail.com>
2021-05-21 12:58:43 -0700

    finished repositories and websocket config files

---
+++ src/main/java/com/f0rgiv/lethani/config/WebSocketSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.f0rgiv.lethani.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        System.out.println("configure websocket");
+        messages
+                .nullDestMatcher().authenticated()
+                .simpDestMatchers("/**").permitAll()
+                .simpSubscribeDestMatchers("/**").permitAll()
+                .anyMessage().permitAll();
+    }
+}
---
+++ src/main/java/com/f0rgiv/lethani/repositories/MobInterface.java
@@ -0,0 +1,4 @@
+package com.f0rgiv.lethani.repositories;
+
+public interface MobInterface {
+}
