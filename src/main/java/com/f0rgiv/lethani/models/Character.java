9cbe08f43d0678bbd935d2de2b6f2f132fd09c6e
James Mansour <jamestmansour@outlook.com>
2021-05-25 09:34:30 -0700

    Implements Character model

--- src/main/java/com/f0rgiv/lethani/models/Character.java
+++ src/main/java/com/f0rgiv/lethani/models/Character.java
@@ -1,4 +1,43 @@
 package com.f0rgiv.lethani.models;
 
+import javax.persistence.*;
+
+@Entity
 public class Character {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+
+    String name; //TODO
+    int xp;
+    int hp;
+
+    @OneToOne(mappedBy = "character")
+    AppUser appUser;
+
+    @ManyToOne
+    @JoinColumn(name = "character_class_id")
+    CharacterClass characterClass;
+
+    //===============Constructors========================
+
+    public Character() {
+    }
+
+    public Character(String name, int xp, int hp, AppUser appUser, CharacterClass characterClass) {
+        this.name = name;
+        this.xp = xp;
+        this.hp = hp;
+        this.appUser = appUser;
+        this.characterClass = characterClass;
+    }
+
+    //=====================Getters====================
+
+    public long getId() {
+        return id;
+    }
+
+    //=====================Setters====================
+
 }
