fc7f82e5e36bdf1ec665ea89858403cd787130cf
James Mansour <jamestmansour@outlook.com>
2021-05-25 09:34:48 -0700

    Implements Mob model

--- src/main/java/com/f0rgiv/lethani/models/Mob.java
+++ src/main/java/com/f0rgiv/lethani/models/Mob.java
@@ -1,4 +1,37 @@
 package com.f0rgiv.lethani.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Mob {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+
+    String name;
+    int hp;
+    int damage;
+
+    //===============Constructors========================
+
+    public Mob() {
+    }
+
+    public Mob(String name, int hp, int damage) {
+        this.name = name;
+        this.hp = hp;
+        this.damage = damage;
+    }
+
+    //=====================Getters====================
+
+    public long getId() {
+        return id;
+    }
+
+    //=====================Setters====================
+
 }
