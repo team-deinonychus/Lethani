64234ab03f5ac43adefcb1f64c9fd3f3689840e5
James Mansour <jamestmansour@outlook.com>
2021-05-26 10:37:37 -0700

    simplify main-class

--- src/main/java/com/f0rgiv/lethani/DemoApplication.java
+++ src/main/java/com/f0rgiv/lethani/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LethaniApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LethaniApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
